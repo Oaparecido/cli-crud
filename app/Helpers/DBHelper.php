db8429dba507d399fb11a5b3176f3f138133e754
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-26 21:57:49 -0300

    ✨ Added function to create table and insert into table

--- app/Helpers/DBHelper.php
+++ app/Helpers/DBHelper.php
@@ -1,10 +1,49 @@
 <?php
 
-
 namespace App\Helpers;
 
+use App\database\Connection;
+use Exception;
+use PDO;
 
 class DBHelper
 {
+    private static $pdo;
+
+    /**
+     * @throws Exception
+     */
+    private static function up()
+    {
+        try {
+            self::$pdo = (new Connection())->connect();
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage(), $exception->getCode());
+        }
+    }
+
+    /**
+     * @param string $assignment
+     * @return bool
+     * @throws Exception
+     */
+    public static function create(string $table, string $assignment): bool
+    {
+        self::up();
+
+        $sql = "CREATE TABLE IF NOT EXISTS $table (
+                id integer primary key autoincrement,
+                assignment char(100) not null 
+                 )";
+
+        try {
+            self::$pdo->exec($sql);
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage(), $exception->getCode());
+        }
+
+        $sql = "INSERT INTO $table (assignment) VALUES (:assignment)";
+        return (self::$pdo->prepare($sql)->execute([$assignment])) ? true : false;
+    }
 
 }
