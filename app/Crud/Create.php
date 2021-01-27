aca196339df114a2fafe1a1af221b4e85cade712
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-26 21:54:17 -0300

    ✨ Create function store, to create table and assignment

--- app/Crud/Create.php
+++ app/Crud/Create.php
@@ -4,7 +4,23 @@
 namespace App\Crud;
 
 
+use App\Helpers\DBHelper;
+use Exception;
+
 class Create
 {
+    private $args = [];
+    public function __construct(array $argv)
+    {
+        $this->args['table_name'] = $argv[2];
+        $this->args['assignment'] = $argv[3];
+    }
 
+    /**
+     * @throws Exception
+     */
+    public function store()
+    {
+        DBHelper::create($this->args['table_name'], $this->args['assignment']);
+    }
 }
