3f87b2cbf6745635835d441a269e556002fd5281
Oaparecido <daniel.aparecido4683@gmail.com>
2021-01-27 20:23:09 -0300

    📝 Update documentation in code

--- app/Crud/Create.php
+++ app/Crud/Create.php
@@ -5,11 +5,20 @@ namespace App\Crud;
 
 
 use App\Helpers\DBHelper;
+use App\Helpers\ResponseHelper;
 use Exception;
 
 class Create
 {
-    private $args = [];
+    /**
+     * @var array
+     */
+    private array $args = [];
+
+    /**
+     * Create constructor.
+     * @param array $argv
+     */
     public function __construct(array $argv)
     {
         $this->args['table_name'] = $argv[2];
